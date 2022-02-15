bdf9879947e72de2541e4a93f3d38b74dbfb3619
ananthithanumoorthy <69436055+ananthithanumoorthy@users.noreply.github.com>
2022-02-15 22:39:05 +0400

    Medical bill app

---
+++ Form2.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace FIRSTMEDICALAPPLICATION
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(489, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pay";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(167, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "prices";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Invoice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIRSTMEDICALAPPLICATION
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 20;
+            int b = 40;
+            int x = Pay(a, b);
+                MessageBox.Show("Paid successfully the amount:"+x);
+        }
+        public int Pay(int a, int b)
+        {
+             int c = a + b;
+            return c;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a= 20;
+            int b = 40;
+            Price(a,b);
+
+        }
+        public void Price(int a,int b)
+        {
+            int c = a + b;
+            MessageBox.Show("amount to be paid is:" + "  " + c);
+        }
+    }
+}
